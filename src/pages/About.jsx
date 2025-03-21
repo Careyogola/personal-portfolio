cb1c779b1b507171af75658579bb29b4e344b681
Carey Ogola <ogolacarey254@gmail.com>
2025-03-21 09:38:02 +0300

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About page!</div>
-  )
-}
+    <div className="dark:bg-gray-950 h-screen w-full flex justify-center items-center p-12">
+      <p className="dark:text-gray-400 font-normal max-w-2xl  lg:text-center">
+        I am a skilled Full Stack Software Engineer with expertise in React, Node.js, MongoDB, and Flutter.  
+        I have a strong background in mobile and web development, building responsive and scalable applications.  
+        With experience in Firebase, Git, and relational databases like PostgreSQL,  
+        I excel in developing both frontend and backend solutions.  
+        My work spans across e-commerce, event management, and art showcase platforms,  
+        demonstrating my versatility in API development, authentication, and UI/UX design.  
+        Additionally, I have experience in admin panel development, cybersecurity, and ERP/CRM systems,  
+        making me a well-rounded software engineer capable of handling complex projects.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
