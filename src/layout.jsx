72cc4acb8727981b3f70b282237581d8fbfcb91a
Carey Ogola <ogolacarey254@gmail.com>
2025-03-26 12:02:55 +0300

    Fragment

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,10 +3,10 @@ import Nav from "./components/Nav"; // Adjust path if necessary
 
 const Layout = () => {
   return (
-    <>
+    <Fragment>
       <Nav />
       <Outlet />
-    </>
+    </Fragment>
   );
 };
 
